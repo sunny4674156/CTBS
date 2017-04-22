322eb4f8971267056380a94d6f7f3a7ad380cbd0
sunny4674156 <h4674156@gmail.com>
2017-04-22 14:26:07 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : '%e4%b8%ad%e8%87%ba%e4%ba%8c%e6%89%8b%e6%9b%b8%e5%9d%8a',
+		wp_ws_url : 'http://ctbs.int.eu.org/wp-appkit-api/%e4%b8%ad%e8%87%ba%e4%ba%8c%e6%89%8b%e6%9b%b8%e5%9d%8a',
+		wp_url : 'http://ctbs.int.eu.org',
+		theme : 'q-android-1.0.4',
+		version : '0.1',
+		app_title : 'CTBS',
+		app_platform : 'android',
+		gmt_offset : 8,
+		debug_mode : 'off',
+		auth_key : 'put your unique phrase here',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
